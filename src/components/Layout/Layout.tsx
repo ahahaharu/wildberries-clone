40fbc7148986e513af9f4c83533649133b12abb2
Andrey <112519966+ahahaharu@users.noreply.github.com>
2026-01-06 17:23:44 +0300

    replace Box with Stack component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Outlet, Link as RouterLink } from 'react-router-dom';
-import { Container, Box } from '@mui/material';
+import { Container, Box, Stack } from '@mui/material';
 import { Header } from '../Header/Header';
 
 const Layout = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Stack sx={{ minHeight: '100vh' }}>
       <Header />
       <Container maxWidth="lg" sx={{ marginTop: '20px' }}>
         <Outlet />
       </Container>
-    </Box>
+    </Stack>
   );
 };
 
